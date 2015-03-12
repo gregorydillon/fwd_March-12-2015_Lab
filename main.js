be5a54485e265d81a66751f37a3b64c5fe40c870
Gregory Dillon <gregorydillon@gmail.com>
2015-03-11 18:30:35 -0700

    small revision

--- main.js
+++ main.js
@@ -11,16 +11,5 @@ var createName = function(first,last){
 
 createName();
 
-var askName = function()
+var askName = function(){}
 
-
-// Example Code
-/*function appendComment(passvar) {
-  //in this case, I have passed a string variable and I return
-  //a string variable. I could return any variable type I want.
-  passvar += ' without your help';
-  return passvar;
-}
-
-var myString = appendComment('I did it');*/
-//myString is now 'I did it without your help'
