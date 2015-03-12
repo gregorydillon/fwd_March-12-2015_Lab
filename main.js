b59811c61df53b738af42a4660f5f62ebb4a4c38
Gregory Dillon <gregorydillon@gmail.com>
2015-03-11 20:56:49 -0700

    basically working

--- main.js
+++ main.js
@@ -1,40 +1,18 @@
 /*Assignment for Week 8*/
-
-
-
-var  first = prompt("What's your first name");
-
-if (first != null) {
-// continue
-
-}
-var  last = prompt("What's your last name");
-
-if (last != null) {
-// continue
-
-}
-
-
-
-
 var createName = function(first,last){
-  return(first+" "+last);
-}
-
-createName();
-
-var askName = function()
+    return(first+" "+last);
+      };
 
 
-// Example Code
-function appendComment(passvar) {
-  //in this case, I have passed a string variable and I return
-  //a string variable. I could return any variable type I want.
-  passvar += ' without your help';
-  return passvar;
-}
+var askName = function(){
+  var  first = prompt("What's your first name");
+  var  last = prompt("What's your last name");
+  var fullName = createName(first,last);
+  alert("Your name is "+ fullName);
+};
 
-var myString = appendComment('I did it');
+askName();
 
-//myString is now 'I did it without your help'
+
+
+
