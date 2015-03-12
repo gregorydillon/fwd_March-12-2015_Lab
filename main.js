6b27be2595c1cb639c8c07a2a912c1841339758c
Gregory Dillon <gregorydillon@gmail.com>
2015-03-11 18:16:02 -0700

    done

---
+++ main.js
@@ -0,0 +1,26 @@
+/*Assignment for Week 8*/
+
+
+var first = ("Greg");
+var last = ("Dillon")
+
+
+var createName = function(first,last){
+  return(first+" "+last);
+}
+
+createName();
+
+var askName = function()
+
+
+// Example Code
+/*function appendComment(passvar) {
+  //in this case, I have passed a string variable and I return
+  //a string variable. I could return any variable type I want.
+  passvar += ' without your help';
+  return passvar;
+}
+
+var myString = appendComment('I did it');*/
+//myString is now 'I did it without your help'
