6152769a6788f4a5b3bebec8ea34c15f5dffad1c
Gregory Dillon <gregorydillon@gmail.com>
2015-03-11 22:18:35 -0700

    working

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ var createName = function(first,last){
 
 
 var askName = function(){
-  var  first = prompt("What's your first name");
-  var  last = prompt("What's your last name");
-  var fullName = createName(first,last);
-  alert("Your name is "+ fullName);
-};
+  while (fullName !=="Greg Dillon"){
+      var  first = prompt("What's your first name");
+      var  last = prompt("What's your last name");
+      var fullName = createName(first, last);
+      alert("Your name is "+ fullName);
+    };
+
+}
 
 askName();
 
